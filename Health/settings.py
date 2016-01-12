1b2e58bcb7629db82caad3f1d541b42123130c29
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2016-01-12 04:14:17 -0500

    init

--- Health/settings.py
+++ Health/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname((os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(f&c7m88fm)+#5&u!fwh1ddr!)iuxq&*0euffd$c)n_3rf)%k2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
