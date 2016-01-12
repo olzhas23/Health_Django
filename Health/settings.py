28f6423c1c8bd9b936fb39adb7fbea3baf7f4d46
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2016-01-11 23:42:46 -0500

    init

--- Health/settings.py
+++ Health/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'Health.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      #  'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'health'),
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
 
     }
 }
