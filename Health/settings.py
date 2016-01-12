1ff30a8e2e4fd7210641f2f34677f1b8561fe6ae
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2016-01-12 07:08:17 -0500

    deploy settings

--- Health/settings.py
+++ Health/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname((os.path.abspath(__file__))
 #SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['https://health-doc-pat.herokuapp.com']
